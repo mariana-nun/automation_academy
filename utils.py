938fcea8ed79d08476c273aae010f5d89f672460
mnunez <mnunez@sparkdigital.com>
2021-09-23 19:57:07 -0300

    Add changes to start using environment variables in CI executions

--- utils.py
+++ utils.py
@@ -1,17 +1,21 @@
 import configparser
 import os
 from configparser import NoOptionError
+import logging
 
 
 def get_capabilities():
     config = configparser.ConfigParser()
     config.read(os.path.join(os.path.dirname(__file__), './setup.cfg'))
-    capabilities = ['os', 'os_version', 'browser', 'browser_version', 'name', 'build', 'project',
+    capabilities = ['os_name', 'os_version', 'browser', 'browser_version', 'name', 'build', 'project',
                     'browserstack.console', 'device']
     desired_capabilities = {}
     for capability in capabilities:
         try:
-            desired_capabilities[capability] = config.get("capabilities", capability)
-        except NoOptionError:
+            if os.environ.get("IS_CI_EXECUTION", "no") == "yes":
+                desired_capabilities[capability.replace("_name", "")] = os.environ[capability]
+            else:
+                desired_capabilities[capability.replace("_name", "")] = config.get("capabilities", capability)
+        except (NoOptionError, KeyError):
             pass
     return desired_capabilities
